2f0459f14d4f992c5370a1a4186d4cbe1e3eb60c
Wataru Ono <wtr0414@gmail.com>
2020-10-23 11:44:40 +0900

    :sparkles: firebaseとの連携が完了

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,39 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+// import { HomeScreenContainer } from './containers/HomeScreenContainer'
+import { firestore } from './plugins/firebase'
 import { HomeScreenContainer } from './containers/HomeScreenContainer'
 
+type TodoProps = {
+  id: string,
+  content: string,
+  completed: boolean,
+}
+
 const App: React.FC = () => {
-  return <HomeScreenContainer />
+
+  const [todos, setTodos] = useState<TodoProps[]>([])
+
+  useEffect(() => {
+    firestore.collection('todos').onSnapshot((docs) => {
+      const allTodos: TodoProps[] = []
+      docs.forEach(doc => {
+        allTodos.push(doc.data() as TodoProps)
+      })
+      setTodos(allTodos)
+    })
+  }, [])
+
+  return (
+    // <HomeScreenContainer />
+    <div>
+      {todos.map(todo => (
+        <div>
+          <p>{todo.id}</p>
+          <p>{todo.content}</p>
+        </div>
+      ))}
+    </div>
+  )
 }
 
 export default App
